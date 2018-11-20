89f806f423e2688592223a550c1e8d5c91bfa86c
armavox <armavox@gmail.com>
2018-11-20 14:18:44 +0300

    week3_started

--- capstone/project_alice/week3_visual_analysis_and_fe.py
+++ capstone/project_alice/week3_visual_analysis_and_fe.py
@@ -69,12 +69,67 @@ PATH_TO_DATA = 'data'
 # 
 # Функция должна возвращать новый DataFrame (как возвращала функция *prepare_train_set*), только признаков должно быть на 4 больше. Порядок, в котором добавляются признаки: *site1*, ... *site10*, *session_timespan*, *#unique_sites*, *start_hour*, *day_of_week* и *user_id* (это видно и чуть ниже по тому, как функция вызывается).
 
+# In[ ]:
+
+
+from collections import Counter
+def prepare_sites_dict(path_to_data, 
+                       sites_dict_file='sites_dict.pkl',
+                       inds_dict_file='ind_to_sites_dict.pkl',
+                       refresh=False,
+                       return_inds_dict=False):
+    """Func to get dictionaries for converting site's name to it's index.
+        If dictionary for data in PATH_TO_DATA has already been compiled, 
+        functions just pickle dict out of files.
+    """
+    def get_dict():
+        full_df = pd.DataFrame(columns=['site']) # UPD deleted 'timestamp'
+        for file in tqdm(glob(path_to_data + '/*'), desc='Preparing sites dict...'):
+            temp_df = pd.read_csv(file, usecols=['site'])  # UPD: added usecols
+            full_df = full_df.append(temp_df, ignore_index=True)
+
+        sites_freq_list = sorted(Counter(full_df.site).items(), 
+                                 key=lambda x: x[1], reverse=True)
+        sites_dict = dict((s, [i, freq]) for i, (s, freq) in enumerate(sites_freq_list, 1))
+        if return_inds_dict:
+            ind_to_sites_dict = dict((val[0], key) for key, val in sites_dict.items())
+            ind_to_sites_dict[0] = 'no_site'
+        else:
+            ind_to_sites_dict = {}
+        
+        # Save dict to file
+        with open(os.path.join(PATH_TO_DATA, sites_dict_file), 'wb') as fout:
+            pickle.dump(sites_dict, fout)
+        if return_inds_dict:
+            with open(os.path.join(PATH_TO_DATA, inds_dict_file), 'wb') as fout:
+                pickle.dump(ind_to_sites_dict, fout)
+            
+        return sites_dict, ind_to_sites_dict
+    
+    try:
+        with open(os.path.join(PATH_TO_DATA, sites_dict_file), 'rb') as input_file:
+            sites_dict = pickle.load(input_file)
+        if return_inds_dict:    
+            with open(os.path.join(PATH_TO_DATA, inds_dict_file), 'rb') as input_file:
+                ind_to_sites_dict = pickle.load(input_file)
+        else:
+            ind_to_sites_dict = {}
+            
+    except FileNotFoundError:
+        sites_dict, ind_to_sites_dict = get_dict()
+        
+    if refresh:
+        sites_dict, ind_to_sites_dict = get_dict()
+        
+    return sites_dict, ind_to_sites_dict
+
+
 # In[3]:
 
 
 def prepare_train_set_with_fe(path_to_csv_files, site_freq_path, feature_names,
                                     session_length=10, window_size=10):
-    ''' ВАШ КОД ЗДЕСЬ '''
+    
 
 
 # **Проверим функцию на игрушечном примере.**
